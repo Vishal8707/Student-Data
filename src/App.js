012fc6dc95f739aedcded517d90667c798013bc6
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-04-20 18:20:18 +0530

    student-info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [submit, setSubmit] = useState([]);
 
-function App() {
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleNum = (e) => {
+    setNumber(e.target.value);
+  };
+  const handlePass = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setSubmit([
+      { name: name, email: email, number: number, password: password },
+    ]);
+  };
+  console.log(submit);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Name</label>
+      <input onChange={handleName} type="name" />
+      <label>Email</label>
+      <input onChange={handleEmail} type="email" />
+      <label>Number</label>
+      <input onChange={handleNum} type="number" />
+      <label>Password</label>
+      <input onChange={handlePass} type="password" />
+      <button onClick={handleSubmit}>Submit</button>
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Number</th>
+          <th>Password</th>
+        </tr>
+
+        {submit.map((x) => (
+          <tr>
+            <td> {x.name} </td>
+            <td> {x.email} </td>
+            <td> {x.number} </td>
+            <td> {x.password} </td>
+          </tr>
+
+        ))}
+
+      </table>
     </div>
   );
 }
-
-export default App;
